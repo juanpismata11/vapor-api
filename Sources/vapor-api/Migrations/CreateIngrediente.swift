f086ff94430e734aa8505de5f1dab574140b37fa
Paulina <paulinaorozcolsb@gmail.com>
2025-06-18 14:04:44 -0600

    modifique la migracion de ingredientes otra vez

--- Sources/vapor-api/Migrations/CreateIngrediente.swift
+++ Sources/vapor-api/Migrations/CreateIngrediente.swift
@@ -3,15 +3,15 @@ import Vapor
 
 struct CreateIngrediente: AsyncMigration {
     func prepare(on database: any Database) async throws {
-        // Capturamos el tipo enum correctamente
-        let unidadMedidaEnum = try await database.enum("unidad_medida_enum")
+        let unidadMedidaEnumBuilder = database.enum("unidad_medida_enum")
             .case("unidad").case("kg").case("lt").case("gr").case("ml")
-            .create()
+
+        try await unidadMedidaEnumBuilder.create()
 
         try await database.schema("ingredientes")
             .field("id_ingrediente", .int, .identifier(auto: true))
             .field("nombre", .string, .required)
-            .field("unidad_medida", .enum(unidadMedidaEnum), .required) // ¡aquí usamos el tipo, no string!
+            .field("unidad_medida", .enum("unidad_medida_enum"), .required)
             .field("stock_actual", .double, .required)
             .field("stock_minimo", .double, .required)
             .field("costo", .double, .required)
@@ -19,7 +19,7 @@ struct CreateIngrediente: AsyncMigration {
             .unique(on: "nombre")
             .create()
     }
-    
+
     func revert(on database: any Database) async throws {
         try await database.schema("ingredientes").delete()
         try await database.enum("unidad_medida_enum").delete()
