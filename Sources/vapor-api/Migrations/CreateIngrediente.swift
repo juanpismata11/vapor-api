a30f2d08c1afc5e59a9f4218b2d5a25f1057a766
Paulina <paulinaorozcolsb@gmail.com>
2025-06-18 14:39:43 -0600

    WE ARE THE CHAMPIONS MY FRIENDS, modifique la migracion ingredientes y lo logre

--- Sources/vapor-api/Migrations/CreateIngrediente.swift
+++ Sources/vapor-api/Migrations/CreateIngrediente.swift
@@ -3,15 +3,20 @@ import Vapor
 
 struct CreateIngrediente: AsyncMigration {
     func prepare(on database: any Database) async throws {
-        let unidadMedidaEnumBuilder = database.enum("unidad_medida_enum")
-            .case("unidad").case("kg").case("lt").case("gr").case("ml")
+        // Creamos el ENUM directamente
+        try await database.enum("unidad_medida_enum")
+            .case("unidad")
+            .case("kg")
+            .case("lt")
+            .case("gr")
+            .case("ml")
+            .create()
 
-        try await unidadMedidaEnumBuilder.create()
-
+        // Usamos el nombre del ENUM como string
         try await database.schema("ingredientes")
             .field("id_ingrediente", .int, .identifier(auto: true))
             .field("nombre", .string, .required)
-            .field("unidad_medida", .enum("unidad_medida_enum"), .required)
+            .field("unidad_medida", .custom("unidad_medida_enum"), .required)
             .field("stock_actual", .double, .required)
             .field("stock_minimo", .double, .required)
             .field("costo", .double, .required)
